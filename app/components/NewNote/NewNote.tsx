106d83eef55b70c34dbe579661a71aa05217971f
b.zakaria <zakaria_bouzangad@gmail.Com>
2024-05-03 12:07:44 +0100

    Surfacing Component Styles

---
+++ app/components/NewNote/NewNote.tsx
@@ -0,0 +1,27 @@
+import styles from './NewNote.css?url';
+
+import { LinksFunction } from "@remix-run/node";
+
+function NewNote() {
+  return (
+    <form method="post" id="note-form">
+      <p>
+        <label htmlFor="title">Title</label>
+        <input type="text" id="title" name="title" required />
+      </p>
+      <p>
+        <label htmlFor="content">Content</label>
+        <textarea id="content" name="content" rows="5" required />
+      </p>
+      <div className="form-actions">
+        <button>Add Note</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewNote;
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+];
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,7 +1,12 @@
+import { LinksFunction } from "@remix-run/node";
+import NewNote , {links as NewNoteStyle} from "~/components/NewNote/NewNote";
+
 export default function NotesPage() {
   return (
-    <div>
-        notes
-    </div>
+    <main>
+      <NewNote />
+    </main>
   )
 }
+
+export const links: LinksFunction = () => [...NewNoteStyle()];
