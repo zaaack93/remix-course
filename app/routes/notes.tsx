9cd6eba105cd5a4abd88f9ba8515274f517c0f98
bouzangad zakaria <bouzangadzakaria@gmail.com>
2024-05-07 17:15:25 +0100

    add laoder to get data

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,22 +1,26 @@
-import { ActionFunctionArgs, LinksFunction, redirect } from "@remix-run/node";
+import { ActionFunctionArgs, LinksFunction, json, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote , {links as NewNoteStyle} from "~/components/NewNote/NewNote";
+import NoteList, {links as NoteListStyle} from "~/components/NoteList/NoteList";
 import { getStoredNotes,storeNotes } from "~/data/notes";
 
 
 export default function NotesPage() {
+  const {notes} = useLoaderData<typeof loader>();
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes}/>
     </main>
   )
 }
 
-export const links: LinksFunction = () => [...NewNoteStyle()];
+export const links: LinksFunction = () => [...NewNoteStyle(),...NoteListStyle()];
 
 
 export const action = async ({request}:ActionFunctionArgs) => {
   const body = await request.formData();
-  
+
   const noteData = {
     id: new Date().toISOString(),
     ...Object.fromEntries(body)
@@ -27,3 +31,8 @@ export const action = async ({request}:ActionFunctionArgs) => {
   await storeNotes(updatedNotes);
   return redirect(`/notes`);
 }
+
+export const loader = async () => {
+  const existingNotes = await getStoredNotes(); 
+  return json({ notes: existingNotes });
+};
